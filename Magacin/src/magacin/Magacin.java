3a92e3c8aea3178db4ffb192c3989295ee4d4548
mihailoslj <samozagit@gmail.com>
2022-03-16 21:54:15 +0100

    Dodata klasa Magacin u paketu magacin; sadrzi liste objekata Knjiga, KancelarijskiMaterijal i KucnaHemija

---
+++ Magacin/src/magacin/Magacin.java
@@ -0,0 +1,14 @@
+package magacin;
+
+import java.util.List;
+
+import artikal.KancelarijskiMaterijal;
+import artikal.Knjiga;
+import artikal.KucnaHemija;
+
+public class Magacin {
+	List<KucnaHemija> listaKucnaHemija = null;
+	List<KancelarijskiMaterijal> listaKancelarijskiMaterijal = null;
+	List<Knjiga> listaKnjigas = null;
+	
+}
